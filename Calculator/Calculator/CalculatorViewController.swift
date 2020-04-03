21074893b32dc5ca27852b9c75525bbfbc417db3
HimalayaRajput0211 <himalaya@vinsol.com>
2020-04-03 23:40:09 +0530

    3 April 2020

--- Calculator/Calculator/CalculatorViewController.swift
+++ Calculator/Calculator/CalculatorViewController.swift
@@ -8,114 +8,168 @@
 
 import UIKit
 class CalculatorViewController: UIViewController {
-    private var didStartEnteringFirstTime = true
+    private var didStartEnteringFirstOperand = true
     private var shouldClearDefaultZero = true
-    private var enteringSecondNumber = false
-    private var firstNumber: String = ""
-    private var secondNumber: String = ""
-    private var numericxpression: String = ""
-    private var arithmeticOperation: ArithmeticOperation?
+    private var isUsingPreviousResult = false
+    private var numericExpression: String = ""
     @IBOutlet private var buttons: [UIButton]!
-    @IBOutlet private weak var display: UILabel!
+    @IBOutlet private weak var calculatorDisplayLabel: UILabel!
+    lazy private var maximumLengthOfOperand: Int = {
+        if let text = calculatorDisplayLabel.text {
+            if Int(text) != nil {
+                return 15
+            }
+            if Double(text) != nil {
+                return 20
+            }
+        }
+        return 15
+    }()
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         circleTheButtons()
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let expression = NSExpression(format: "2.0/3.0")
-        if let result = expression.expressionValue(with: nil, context: nil) as? Double {
-              if let intResult = Int(exactly: result ) {
-                       print(intResult)
-              } else {
-                print(result)
-            }
-            
-        }
-      
-    
-         
-        
-       
-    }
-    
+   
     @IBAction private func numberButtonTapped(_ sender: UIButton) {
-        if !didStartEnteringFirstTime {
-            display.text = ""
-            secondNumber = ""
-            didStartEnteringFirstTime = true
+        if isUsingPreviousResult {
+            numericExpression = ""
+            isUsingPreviousResult = false
         }
         if shouldClearDefaultZero {
-            display.text = ""
+            calculatorDisplayLabel.text = ""
             shouldClearDefaultZero = false
         }
-        if let previousText = display.text, let currentText = sender.currentTitle {
+        if !didStartEnteringFirstOperand {
+            calculatorDisplayLabel.text = ""
+            didStartEnteringFirstOperand = true
+        }
+        if var previousText = calculatorDisplayLabel.text, let currentText = sender.currentTitle {
+            if previousText == "inf" {
+                previousText = ""
+            }
             let newText = previousText + currentText
-            if isValidCount(for: newText) {
-                display.text = newText
-                if enteringSecondNumber {
-                    secondNumber = newText
-                }
+            if previousText.count < maximumLengthOfOperand {
+                appendNumericValueToExpression(currentText)
+            }
+            if isValidTextToDisplay(for: newText) {
+                calculatorDisplayLabel.text = newText
             }
         }
     }
     
     @IBAction private func commandButtonTapped(_ sender: UIButton) {
-        if let text = display.text {
-            didStartEnteringFirstTime = false
-            switch sender.tag {
-            case 0:
-                if let operation = arithmeticOperation {
-                    let exprssion = Expression(expression: firstNumber + " " + operation.rawValue + " " + secondNumber)
-                    if let result = exprssion.evaluteExpression() {
-                        display.text = result
-                        arithmeticOperation = nil
-                        enteringSecondNumber = false
+        switch sender.tag {
+        case 0:
+            if isValidExpressionToEvaluate() {
+                configureNumericExpressionForIntegerOperand()
+                let expression = NSExpression(format: numericExpression)
+                if let result = expression.expressionValue(with: nil, context: nil) as? Double {
+                    if let inetgerResult = Int(exactly: result ) {
+                        calculatorDisplayLabel.text = "\(inetgerResult)"
+                        numericExpression = "\(inetgerResult)"
+                        isUsingPreviousResult = true
                     } else {
-                        clearCalculator()
+                        calculatorDisplayLabel.text = "\(result)"
+                        if result.isInfinite {
+                            numericExpression = ""
+                        } else {
+                            numericExpression = "\(result)"
+                            isUsingPreviousResult = true
+                        }
                     }
                 }
-            case 1:
-                arithmeticOperation = .addition
-                firstNumber = text
-                appendOperand(text)
-                appendOperator("+")
-                enteringSecondNumber = true
-            case 2:
-                arithmeticOperation = .subtraction
-                firstNumber = text
-                enteringSecondNumber = true
-            case 3:
-                arithmeticOperation = .multiplication
-                firstNumber = text
-                enteringSecondNumber = true
-            case 4:
-                arithmeticOperation = .division
-                firstNumber = text
-                enteringSecondNumber = true
-            case 5:
-                clearCalculator()
-            default:
-                break
             }
+        case 1:
+            if isValidOperand() {
+                appendOperator("+")
+                isUsingPreviousResult = false
+                didStartEnteringFirstOperand = false
+            }
+        case 2:
+            if isValidOperand() {
+                appendOperator("-")
+                isUsingPreviousResult = false
+                didStartEnteringFirstOperand = false
+            }
+        case 3:
+            if isValidOperand() {
+                appendOperator("*")
+                isUsingPreviousResult = false
+                didStartEnteringFirstOperand = false
+            }
+        case 4:
+            if isValidOperand() {
+                appendOperator("/")
+                isUsingPreviousResult = false
+                didStartEnteringFirstOperand = false
+            }
+        case 5:
+            clearCalculator()
+        default:
+            break
         }
     }
-    func appendOperand(_ operand: String) {
-        if let doubleOperand = Double(operand) {
-             numericxpression += "\(doubleOperand)"
+    
+    private func appendNumericValueToExpression(_ numericValue: String) {
+        if numericValue == "." {
+            if let displayText = calculatorDisplayLabel.text ,let _ = displayText.last {
+                if isValidTextToDisplay(for: displayText + numericValue) {
+                    numericExpression += numericValue
+                }
+            } else {
+                numericExpression += "0."
+                calculatorDisplayLabel.text = "0."
+            }
+        } else {
+            numericExpression += numericValue
         }
+        
     }
-    func appendOperator(_ arithmeticOperator: Character) {
-        if let lastCharacter = numericxpression.last {
+    
+    private func appendOperator(_ arithmeticOperator: Character) {
+        if let lastCharacter = numericExpression.last {
             if !lastCharacter.isNumber {
-                numericxpression.removeLast()
-                numericxpression.append(arithmeticOperator)
+                numericExpression.removeLast()
+                numericExpression.append(arithmeticOperator)
+            } else {
+                configureNumericExpressionForIntegerOperand()
+                numericExpression.append(arithmeticOperator)
             }
         }
     }
     
-    private func isValidCount(for text: String) -> Bool {
+    private func isValidExpressionToEvaluate() -> Bool {
+        if let lastCharacter = numericExpression.last {
+            if lastCharacter.isNumber {
+                return true
+            } else {
+                return false
+            }
+        }
+        return false
+    }
+    
+    private func configureNumericExpressionForIntegerOperand() {
+        if let operand = calculatorDisplayLabel.text {
+            if Int(operand) != nil {
+                numericExpression += ".0"
+            }
+        }
+    }
+    
+    private func isValidOperand() -> Bool {
+        if let operand = calculatorDisplayLabel.text {
+            if operand.last == "." {
+                return false
+            } else {
+                return true
+            }
+        }
+        return false
+    }
+    
+    private func isValidTextToDisplay(for text: String) -> Bool {
         if Int(text) != nil  {
             if text.count <= 15 {
                 return true
@@ -134,11 +188,10 @@ class CalculatorViewController: UIViewController {
     }
     
     private func clearCalculator() {
-        display.text = "0"
-        firstNumber = ""
-        secondNumber = ""
-        didStartEnteringFirstTime = true
-        enteringSecondNumber = false
+        calculatorDisplayLabel.text = "0"
+        numericExpression = ""
+        didStartEnteringFirstOperand = true
+        isUsingPreviousResult = false
         shouldClearDefaultZero = true
     }
     
@@ -147,7 +200,7 @@ class CalculatorViewController: UIViewController {
         view.alpha = 0.0
         coordinator.animate(alongsideTransition: { _ in }) { _ in
             UIView.animate(withDuration: 0.4) {
-                 self.view.alpha = 1.0
+                self.view.alpha = 1.0
             }
         }
     }
@@ -160,11 +213,4 @@ class CalculatorViewController: UIViewController {
             button.layer.cornerRadius = cornerRadius
         }
     }
-}
-
-enum ArithmeticOperation: String {
-    case addition = "+"
-    case subtraction = "-"
-    case multiplication = "*"
-    case division = "/"
 }
